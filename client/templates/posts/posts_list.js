822b7c3bb8e903fe9910d7ad17e9684cb4382f5f
juan emmanuel diaz <juan emmanuel diaz>
2017-08-19 14:10:42 -0300

    'commit7mo'

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,24 +1,24 @@
-var postsData=[
-  {
-  title: 'Introducing Telescope',
-  url: 'http://sachagreif.com/introducing-telescope/'
-  },
-  {
-  title: 'Meteor',
-  url: 'http://meteor.com'
-  },
-  {
-  title: 'The Meteor Book',
-  url: 'http://themeteorbook.com'
-  }
-];
+// var postsData=[
+//   {
+//   title: 'Introducing Telescope',
+//   url: 'http://sachagreif.com/introducing-telescope/'
+//   },
+//   {
+//   title: 'Meteor',
+//   url: 'http://meteor.com'
+//   },
+//   {
+//   title: 'The Meteor Book',
+//   url: 'http://themeteorbook.com'
+//   }
+// ];
 
 // Aqui se define el ayudante
-Template.postsList.helpers({
-  posts:function(){
-    return Posts.find({},{sort:{submitted:-1}});
-  }
-});
+// Template.postsList.helpers({
+//   posts:function(){
+//     return Posts.find({},{sort:{submitted:-1}});
+//   }
+// });
 
 //es lo mismo escrito de otra forma
 // Template.postsList.helpers({
